c89fb69a779f79221d55549e10a615871042afef
Ameera-Helal <ameera_m_helal@hotmail.com>
2020-10-16 01:09:00 +0200

    add checkCortType method

--- app/src/main/java/com/example/myrecipes/MainActivity.java
+++ app/src/main/java/com/example/myrecipes/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.SearchView;
 import android.widget.Toast;
 
@@ -45,6 +49,7 @@ public class MainActivity extends AppCompatActivity  {
 
 
 
+        checkSorting(isSortByFats,isSortByCalory);
         apiRequest= AppUtil.getAPIRequest();
 
 
@@ -59,38 +64,8 @@ public class MainActivity extends AppCompatActivity  {
            @Override
            public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
               List<Recipe> recipesList = response.body();
-
-
-
-               recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this, recipesList);
-               recyclerView.setHasFixedSize(true);
-               linearLayoutManager = new LinearLayoutManager(MainActivity.this);
-               recyclerView.setLayoutManager(linearLayoutManager);
-               recyclerView.setItemAnimator(new DefaultItemAnimator());
-               recyclerView.setAdapter(recyclerViewAdapter);
-               recyclerViewAdapter.notifyDataSetChanged();
-
-               searchView.setActivated(true);
-               searchView.setQueryHint("Search by the Recipe Name");
-               searchView.onActionViewExpanded();
-               searchView.setIconified(false);
-               searchView.clearFocus();
-
-               searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                   @Override
-                   public boolean onQueryTextSubmit(String query) {
-                       return false;
-                   }
-
-                   @Override
-                   public boolean onQueryTextChange(String newText) {
-                       recyclerViewAdapter.getFilter().filter(newText);
-                       Log.d("SEARCH", "onQueryTextChange: "+newText);
-                       return false;
-                   }
-               });
-
-
+              recyclerViewConfig(MainActivity.this,recipesList);
+              searchBarConfig();
                for (int index =0 ; index< recipesList.size();index++)
                    Log.d("FetchedRecipes", "onResponse: "+ recipesList.get(index));
            }
@@ -102,6 +77,127 @@ public class MainActivity extends AppCompatActivity  {
 
            }
        });
+    }
+
+
+    public void recyclerViewConfig(Context context, List<Recipe> list)
+    {
+        recyclerViewAdapter = new RecyclerViewAdapter(context, list);
+        recyclerView.setHasFixedSize(true);
+        linearLayoutManager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerViewAdapter.notifyDataSetChanged();
+
+    }
+
+    public void searchBarConfig()
+    {
+        searchView.setActivated(true);
+        searchView.setQueryHint("Search by the Recipe Name");
+        searchView.onActionViewExpanded();
+        searchView.setIconified(false);
+        searchView.clearFocus();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                recyclerViewAdapter.getFilter().filter(newText);
+                Log.d("SEARCH", "onQueryTextChange: "+newText);
+                return false;
+            }
+        });
+
+    }
+
+    public String checkSorting(final CheckBox isSortByFats, final CheckBox isSortByCalory)
+    {
+        SharedPreferences shared = getSharedPreferences(AppUtil.APP_INFO,MODE_PRIVATE);
+        String sortType= shared.getString(AppUtil.SORT_TYPE,"").trim();
+
+        if (sortType.contentEquals(AppUtil.FATS))
+        {
+            isSortByFats.setChecked(true);
+
+        }
+
+        else if (sortType.contentEquals(AppUtil.CALORIES))
+        {
+            isSortByCalory.setChecked(true);
+        }
+
+
+        isSortByFats.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+
+                if(isSortByFats.isChecked() && isSortByFats.isPressed())
+                {
+                    isSortByCalory.setChecked(false);
+                    SharedPreferences sharedPreferences = getSharedPreferences(AppUtil.APP_INFO,MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(AppUtil.SORT_TYPE,AppUtil.FATS);
+                    editor.apply();
+                    Log.d("SORT_TYPE", "onCheckedChanged: "+ "fats");
+
+
+                }
+                else if (!isSortByFats.isChecked() && isSortByFats.isPressed())
+                {
+                    SharedPreferences sharedPreferences = getSharedPreferences(AppUtil.APP_INFO,MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(AppUtil.SORT_TYPE,null);
+                    editor.apply();
+
+                }
+
+
+
+            }
+        });
+
+
+
+        isSortByCalory.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+
+                if(isSortByCalory.isChecked() && isSortByCalory.isPressed())
+                {
+                    isSortByFats.setChecked(false);
+                    SharedPreferences sharedPreferences = getSharedPreferences(AppUtil.APP_INFO,MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(AppUtil.SORT_TYPE,AppUtil.CALORIES);
+                    editor.apply();
+                    Log.d("SORT_TYPE", "onCheckedChanged: "+ "Calory");
+
+
+                }
+                else if (!isSortByCalory.isChecked() && isSortByCalory.isPressed())
+                {
+                    SharedPreferences sharedPreferences = getSharedPreferences(AppUtil.APP_INFO,MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(AppUtil.SORT_TYPE,null);
+                    editor.apply();
+
+                }
+
+
+
+            }
+        });
+
+
+        Log.d("History", "checkSorting: "+sortType);
+        return sortType;
     }
 }
 
--- app/src/main/java/com/example/myrecipes/util/AppUtil.java
+++ app/src/main/java/com/example/myrecipes/util/AppUtil.java
@@ -5,6 +5,10 @@ import com.example.myrecipes.Network.RetrofitClient;
 
 public final class AppUtil {
     public static final String BASE_URL = "https://hf-android-app.s3-eu-west-1.amazonaws.com/";
+    public static final String APP_INFO="app_info";
+    public static final String SORT_TYPE="sort_type";
+    public static final String FATS="fats";
+    public static final String CALORIES="calory";
 
 
     public static ApiRequest getAPIRequest() {
